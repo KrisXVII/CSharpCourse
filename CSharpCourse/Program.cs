10c66e3156f1cc93a66f936c6f4b407915cef410
Cristian Radulescu <kristianradulescu@gmail.com>
2025-09-27 16:50:59 +0200

    test: added interpolation example

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -1,13 +1,21 @@
 ﻿
 using System; // not necessary
+using Spectre.Console;
 
 namespace CSharpCourse
 {
     class Beninging
-    {
+    { 
         private static void Main(string[] args)
         {
-            Console.WriteLine("Ini thi beninging");
+            string alpaca;
+            alpaca = "alpaca";
+            int sas = 8;
+            const int constant = 12;
+            Console.WriteLine($"{alpaca} {sas}");
+            Console.WriteLine(sas + constant);
+            AnsiConsole.WriteLine("Ini thi beninging");
+            // Console.Read(); // doesn't end the process after execution
         }
     }
 }
