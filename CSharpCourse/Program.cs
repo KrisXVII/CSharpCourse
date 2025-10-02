bf74810dc343820e5f8320eb1b886aac2366c687
Cristian Radulescu <kristianradulescu@gmail.com>
2025-10-02 16:41:03 +0200

    chore: added function example

--- CSharpCourse/Program.cs
+++ CSharpCourse/Program.cs
@@ -11,11 +11,20 @@ namespace CSharpCourse
             string alpaca;
             alpaca = "alpaca";
             int sas = 8;
-            const int constant = 12;
-            Console.WriteLine($"{alpaca} {sas}");
-            Console.WriteLine(sas + constant);
+            int num1 = 5, num2 = 10;
+            int sum = num1 + num2;
+            AnsiConsole.WriteLine(sum);
+            const int num = 12;
+            AnsiConsole.WriteLine($"{alpaca} {sas}");
+            AnsiConsole.WriteLine(sas + num);
             AnsiConsole.WriteLine("Ini thi beninging");
+            AnsiConsole.WriteLine(Sum(1.2, 3));
             // Console.Read(); // doesn't end the process after execution
+        }
+
+        private static double Sum(double x, int y)
+        {
+            return x + y;
         }
     }
 }
