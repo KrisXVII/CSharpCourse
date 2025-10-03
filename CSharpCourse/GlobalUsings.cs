5c7ebaad7b52a2725919b5f4be7f57ec0311679e
Cristian Radulescu <kristianradulescu@gmail.com>
2025-10-03 14:45:15 +0200

    feat: added global usings file for global dependencies

---
+++ CSharpCourse/GlobalUsings.cs
@@ -0,0 +1,6 @@
+
+global using Newtonsoft.Json;
+global using Spectre.Console;
+global using System.Collections;
+global using System.Collections.Generic;
+global using static CSharpCourse.JsonHelper;
